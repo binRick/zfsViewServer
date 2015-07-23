de0b365edb774e2f05be87a73b4f856e01a13e4b
Rick <rick@Ricks-MacBook-Air.local>
2015-07-22 23:41:49 -0600

    i

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var path = require('path');
+//curl -X GET "http://localhost:3009/api/multifetch?localFs=/api/Server/cetus/tank_9894340/FilesystemSnapshots/latest,ts&remoteFs=/api/Server/beo/tank_Snapshots_cetus_tank_9894340/FilesystemSnapshots/latest,ts"
+var path = require('path'),
+multifetch = require('multifetch'),
+
+    cors = require('cors');
 
 var express = require('express');
 var app = require('express')(),
@@ -7,18 +11,20 @@ var app = require('express')(),
 var port = process.env.PORT || 3009;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(express.static(path.join(__dirname, 'public_html')));
 
-
+app.get('/api/multifetch', multifetch({ headers: false }));
 app.get('/api/SnapshotServers', routes.SnapshotServers);
 app.get('/api/Server/:server/Pools', routes.serverPools);
 app.get('/api/Server/:server/:pool/Filesystems', routes.serverPoolFilesystems);
 app.get('/api/Server/:server/:fs/FilesystemInfo/:fields', routes.serverFilesystemInfo);
-app.get('/api/Server/:server/:fs/FilesystemSnapshots', routes.serverFilesystemSnapshots);
+app.get('/api/Server/:server/:fs/FilesystemSnapshots/:filter?', routes.serverFilesystemSnapshots);
+app.get('/api/Server/:server/:pool/SnapshotServers', routes.serverPoolSnapshotServers);
 
 
 
--- routes.js
+++ routes.js
@@ -1,16 +1,59 @@
 var zfsGroup = require(__dirname + '/../zfsGroupInfo');
+var moment = require('moment');
+var trim = require('trim');
+
+var _ = require('underscore');
+
+var md5 = require('md5');
+var CachemanFile = require('cacheman-file');
 var zfsFilesystemViews = require(__dirname + '/../zfsFilesystemViews');
 
+var SnapFilter = function(Snapshot) {
+    return String(Snapshot).split('@').length == 2;
+
+
+};
+
+
+var Cache = new CachemanFile();
+
+var serverSnapshots = function(Server, CB) {
+    var Setup = zfsFilesystemViews.Snapshot.Servers.List;
+    Setup.server = Server,
+        Setup.bin = 'zfs';
+    Setup.ttl = 3600;
+    Setup.args = 'list -H -o name -t snap';
+    Setup.matches = [];
+    Setup.key = md5(Setup.server + Setup.bin + Setup.args);
+    Cache.get(Setup.key, function(e, Cached) {
+        if (e) throw e;
+        if (Cached) {
+            console.log('cached hit');
+            return CB(Cached.filter(SnapFilter));
+        } else {
+            console.log('cache miss');
+            zfsGroup(Setup, function(e, Snaps) {
+                if (e) throw e;
+                Setup.cache.set(Setup.key, Snaps, Setup.ttl, function(e) {
+                    console.log('cache set');
+                    return CB(Snaps.filter(SnapFilter));
+                });
+            });
+        }
+    });
+};
+
+
 module.exports = {
-    serverFilesystemSnapshots: function(req, res) {
+    serverPoolSnapshotServers: function(req, res) {
         var Setup = zfsFilesystemViews.Snapshot.Servers.List;
         Setup.server = req.params.server;
         Setup.bin = 'zfs';
-	var fields = req.params.fields;
-	req.params.fs = req.params.fs.replace(/_/g,'/');
-        Setup.args = 'list -H -o name -t snap';
-        Setup.matches = ['^'+req.params.fs+'@'];
-        Setup.field = false;
+        if (req.params.fs)
+            req.params.fs = req.params.fs.replace(/_/g, '/');
+        Setup.args = 'list -H -o name';
+        Setup.matches = ['^' + req.params.pool + '/Snapshots/[a-z]*.[a-z]/tank$'];
+        Setup.field = 2;
         zfsGroup(Setup, function(e, Filesystems) {
             if (e) {
                 res.code(500);
@@ -20,13 +63,46 @@ module.exports = {
             return res.json(Filesystems);
         });
     },
+    serverFilesystemSnapshots: function(req, res) {
+        var Setup = zfsFilesystemViews.Snapshot.Servers.List;
+        Setup.server = req.params.server;
+        Setup.bin = 'zfs';
+        var fields = req.params.fields;
+        req.params.fs = req.params.fs.replace(/_/g, '/');
+        Setup.args = 'list -H -o name -t snap';
+        Setup.matches = ['^' + req.params.fs + '@'];
+        Setup.field = false;
+        serverSnapshots(Setup.server, function(Snapshots) {
+            Snapshots = Snapshots.filter(function(S) {
+                return S.split(req.params.fs).length == 2;
+            });
+            if (req.params.filter) {
+                req.params.filter = req.params.filter.split(',');
+                if (_.contains(req.params.filter, 'latest'))
+                    Snapshots = [_.last(Snapshots)];
+                if (_.contains(req.params.filter, 'ts'))
+                    Snapshots = Snapshots.map(function(s) {
+                        return trim(_.last(s.split('zfs-auto-snap_daily-')));
+                    }).map(function(s) {
+                        var m = moment(s, 'YYYY-MM-DD-hhmm');
+                        return {
+                            str: s,
+                            moment: m,
+                            ts: m.unix(),
+
+                        }
+                    });
+            }
+            return res.json(Snapshots);
+        });
+    },
     serverFilesystemInfo: function(req, res) {
         var Setup = zfsFilesystemViews.Snapshot.Servers.List;
         Setup.server = req.params.server;
         Setup.bin = 'zfs';
-	var fields = req.params.fields;
-	req.params.fs = req.params.fs.replace(/_/g,'/');
-        Setup.args = 'get -H -p -o value '+req.params.fields+' '+req.params.fs;
+        var fields = req.params.fields;
+        req.params.fs = req.params.fs.replace(/_/g, '/');
+        Setup.args = 'get -H -p -o value ' + req.params.fields + ' ' + req.params.fs;
         Setup.matches = [''];
         Setup.field = false;
         zfsGroup(Setup, function(e, Filesystems) {
